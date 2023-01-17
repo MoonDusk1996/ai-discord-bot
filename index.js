433a6431a6f4b2726e2d3fc42f6df47a4cc196b8
Dusk <91294230+MoonDusk1996@users.noreply.github.com>
2023-01-17 15:02:27 -0400

    refac

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+//imports
 const fs = require("node:fs");
 const path = require("node:path");
-
 const {
   Client,
   Collection,
   Events,
   GatewayIntentBits,
   ActivityType,
+  EmbedBuilder,
 } = require("discord.js");
 const dotenv = require("dotenv");
-const dmChatSpan = require("./templates/dm/commands.json");
 dotenv.config();
 const client = new Client({
   intents: [
@@ -20,7 +20,11 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
+const dmChatSpan = require("./templates/dm/noPremiumUser.json");
+const rollDice = require("./services/rollDice");
+const openai = require("./services/fetchOpenai");
 
+//discord config
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
@@ -33,6 +37,7 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
+//once on start
 client.once(Events.ClientReady, () => {
   console.log(
     `${client.user.username} está ${client.presence.status} em ${client.guilds.cache.size} servidores no discord!`
@@ -40,35 +45,7 @@ client.once(Events.ClientReady, () => {
   client.user.setActivity("o Brasil", { type: ActivityType.Playing });
 });
 
-client.on(Events.MessageCreate, (message) => {
-  if (message.guild == "363504194453241866") {
-    if (
-      message.content.includes("corno") ||
-      message.content.includes("boi") ||
-      message.content.includes("gado") ||
-      message.content.includes("vaca") ||
-      message.content.includes("chifre") ||
-      message.content.includes("chifrudo") ||
-      message.content.includes("chifrou") ||
-      message.content.includes("traiu") ||
-      message.content.includes("test")
-    ) {
-      message.react("<:gado:474085129061662721>");
-    }
-    if (
-      message.content.includes("lol") ||
-      message.content.includes("lolzinho") ||
-      message.content.includes("vava") ||
-      message.content.includes("gay") ||
-      message.content.includes("viado") ||
-      message.content.includes("bixa") ||
-      message.content.includes("<@&1016783676526301346>")
-    ) {
-      message.react("🏳️‍🌈");
-    }
-  }
-});
-
+//when on
 client.on(Events.InteractionCreate, async (interaction) => {
   const notification = client.channels.cache.get(
     process.env.CHANNEL_NOTIFICATION_ID
@@ -84,6 +61,75 @@ client.on(Events.InteractionCreate, async (interaction) => {
     default: //guid channel
       await command.execute(interaction, client, notification);
       break;
+  }
+});
+
+//when on premium server
+client.on(Events.MessageCreate, async (message) => {
+  if (message.guild == process.env.PREMIUM_GUILD) {
+    const messagedata = message.content.toLowerCase();
+    const percent = 15;
+
+    if (message.content.includes(`<@${process.env.DISCORD_APP_ID}>`)) {
+      if (message.reference) {
+        const replyto = await message.channel.messages.fetch(
+          message.reference.messageId
+        );
+        const messageReference = replyto.content.replace(
+          `<@${process.env.DISCORD_APP_ID}>`,
+          ""
+        );
+
+        const prompt = `${message.content}: "${messageReference}"`.replace(
+          `<@${process.env.DISCORD_APP_ID}>`,
+          ""
+        );
+        const response = await openai(prompt, 1000);
+        console.log(response);
+        message.reply(response);
+      }
+    }
+
+    //react emoji
+    if (rollDice(percent)) {
+      if (
+        messagedata.includes("corno") ||
+        messagedata.includes("cornos") ||
+        messagedata.includes("boi") ||
+        messagedata.includes("gado") ||
+        messagedata.includes("vaca") ||
+        messagedata.includes("chifre") ||
+        messagedata.includes("chifrudo") ||
+        messagedata.includes("chifrou") ||
+        messagedata.includes("traiu")
+      ) {
+        message.react("<:gado:474085129061662721>"); //gado emoji
+      }
+      if (
+        messagedata.includes("lol") ||
+        messagedata.includes("lolzinho") ||
+        messagedata.includes("vava") ||
+        messagedata.includes("<@&1016783676526301346>")
+      ) {
+        message.react("🏳️‍🌈"); //gay flag emoji
+      }
+      if (
+        messagedata.includes("travesti") ||
+        messagedata.includes("ancap") ||
+        messagedata.includes("caneta") ||
+        messagedata.includes("ateu")
+      ) {
+        message.react("<:3x4:416104479176392707>"); //caneta emoji
+      }
+      if (
+        messagedata.includes("seven") ||
+        messagedata.includes("frango") ||
+        messagedata.includes("galinha") ||
+        messagedata.includes("frangos")
+      ) {
+        message.react("<:Seven:364212708352196618>>"); // seven emoji
+      }
+    }
   }
 });
 
